497ad06cebc4a082c466a8cd22e3470b5ec87f14
Lucas <lucasdearaujo.brandao@gmail.com>
2023-08-10 22:26:30 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner'; // importantdo o nosso componente
 
 function App() {
   return (
     <div className="App">
+      <Banner /> {/* aqui estamos chamando nosso componente criado */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
