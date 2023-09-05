aa4570c6fd6add6173134fdbf345f37c409b7170
Lucas <lucasdearaujo.brandao@gmail.com>
2023-09-04 23:09:40 -0300

    update

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+        <div>
+            <div>
+                <img src='https://github.com/lucas-code42.png' alt=''></img>
+            </div>
+            <div>
+                <h4>Lucas</h4>
+                <h5>Dev backend</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Colaborador;
